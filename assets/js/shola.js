bdc0bae1353ca65f6d62957d6f62fa32b7245637
Joel Omojefe <joelomojefe@gmail.com>
2022-07-10 16:11:36 +0100

    Slides fix

--- assets/js/shola.js
+++ assets/js/shola.js
@@ -1,3 +1,20 @@
-function about(){
-    window.location="assets/html/about.html";
+function about() {
+  window.location = "assets/html/about.html";
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; slides.length > i; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
